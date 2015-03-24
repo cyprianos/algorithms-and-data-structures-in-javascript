288195bf26fba37f9a4bf9a7784c78e04b86f0e1
Cyprian Gepfert cgepfert@gmail.com <cgepfert@gmail.com>
2015-03-24 23:55:58 +0100

    Stack Linked List

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-var LL= require('./lib/LinkedList'),
-    Node = LL.Node,
-    LinkedList = LL.LinkedList;
+var LinkedList = require('./lib/LinkedList');
+var StackLinkedList = require('./lib/StackLinkedList');
 
 
 var list = new LinkedList();
@@ -10,3 +9,14 @@ list.insert(40);
 list.insert(1);
 console.log('size: ', list.size());
 list.traverseLinkedList();
+
+
+var stack = new StackLinkedList();
+stack.push(10);
+stack.push(3);
+
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.size());
